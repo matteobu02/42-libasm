8075f0adc419dcde5985e6ac51f75ee34ae6da77
mbucci <mbucci@student.s19.be>
2023-06-10 04:24:34 +0200

    ft_strcmp done

--- lib/srcs/ft_strcmp.s
+++ lib/srcs/ft_strcmp.s
@@ -1,24 +1,31 @@
 ; int ft_strcmp(const char *s1, const char *s2);
 
 section .text
-	global ft_strcmp
+	global _ft_strcmp
 
-;ft_strcmp:
-;	; prologue
-;	push rbp
-;	mov rbp, rsp
-;
-;	xor rax, rax	; int i = 0;
-;	jmp loop
-;
-;increment:
-;	inc rax
-;
-;loop:
-;	cmp byte [rdi + rax], [rsi + rax]
-;
-;	je end
-;	and byte [rdi + rax], [rsi + rax]
-;	jz end
-;
-;end:
+_ft_strcmp:
+	; prologue
+	push rbp
+	mov rbp, rsp
+
+	mov rcx, -1
+
+L1:
+	inc rcx
+	cmp BYTE [rdi + rcx], 0
+	je endfunc
+	cmp BYTE [rsi + rcx], 0
+	je endfunc
+	mov al, BYTE [rdi + rcx]
+	cmp al, BYTE [rsi + rcx]
+	je L1
+
+endfunc:
+	movzx rax, BYTE [rdi + rcx]
+	movzx rbx, BYTE [rsi + rcx]
+	sub rax, rbx
+
+	; epilogue
+	mov rsp, rbp
+	pop rbp
+	ret
