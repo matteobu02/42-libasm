db52296e74bdd2e1170fda63bc27a856d952cf0e
mbucci <mbucci@student.s19.be>
2023-06-11 17:13:32 +0200

    ft_strchr_asm works fine + changed atoi_base_c layout

--- lib/bonus/ft_atoi_base_bonus.c
+++ lib/bonus/ft_atoi_base_bonus.c
@@ -1,41 +1,19 @@
+#include <ctype.h>
 #include <string.h>
-
-int ft_isspace(int c)
-{
-	return (c == 32 || (c > 8 && c < 14));
-}
-
-char *ft_strchr(const char *s, int c)
-{
-	int i = -1;
-	while (s[++i])
-		if (s[i] == c)
-			return (char *)(s + 1);
-	if (!c)
-		return (char *)(s + 1);
-	return 0;
-}
-
-int check_base(const char *b)
-{
-	if (strlen(b) < 2)
-		return 1;
-	while (*b)
-	{
-		if (ft_isspace(*b) || (*b == '+') || (*b == '-') )
-			return 1;
-		if (ft_strchr(b + 1, *b))
-			return 1;
-		++b;
-	}
-	return 0;
-}
 
 int	ft_atoi_base(const char *s, const char *base)
 {
-	if (!s || !base || check_base(base))
+	int base_len = strlen(base);
+	if (base_len < 2)
 		return 0;
-	while (ft_isspace(*s))
+	while (*base)
+	{
+		if (isspace(*base) || (*base == '+') || (*base == '-') || strchr(base + 1, *base))
+			return 0;
+		++base;
+	}
+	base -= base_len;
+	while (isspace(*s))
 		++s;
 	int sign = 1;
 	while (*s == '-' || *s == '+')
@@ -45,9 +23,8 @@ int	ft_atoi_base(const char *s, const char *base)
 		++s;
 	}
 	int ret = 0;
-	int base_len = strlen(base);
 	char *found;
-	while (*s && (found = ft_strchr(base, *s)))
+	while (*s && (found = strchr(base, *s)))
 	{
 		ret = ret * base_len + (found - base);
 		++s;
--- lib/bonus/ft_atoi_base_bonus.s
+++ lib/bonus/ft_atoi_base_bonus.s
@@ -1,27 +1,31 @@
-; int ft_atoi_base(const char *str, const char *base);
-
 section .text
 	global _ft_atoi_base
-	global ft_strchr
 	extern _ft_strlen
 
-; int isspace(int c);
-;ft_isspace:
-;	cmp rdi, 32
-;	je .isspace_true
-;	cmp rdi, 9
-;	jl .isspace_false
-;	cmp rdi, 13
-;	jg .isspace_false
-;
-;.isspace_true:
-;	mov rax, 1
-;	ret
-;
-;.isspace_false:
-;	xor rax, rax
-;	ret
+; int ft_atoi_base(const char *str, const char *base);
+_ft_atoi_base:
+	
 
+.atoi_error:
+	xor rax, rax
+	ret
+
+; int isspace(int c);
+ft_isspace:
+	cmp rdi, 32
+	je .isspace_true
+	cmp rdi, 9
+	jl .isspace_false
+	cmp rdi, 13
+	jg .isspace_false
+
+.isspace_true:
+	mov rax, 1
+	ret
+
+.isspace_false:
+	xor rax, rax
+	ret
 
 ; char *ft_strchr(const char *s, int c);
 ft_strchr:
@@ -37,5 +41,5 @@ ft_strchr:
 	ret
 
 .strchr_found:
-	mov rax, [rdi + rcx]
+	lea rax, [rdi + rcx]
 	ret
