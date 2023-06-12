3ebc4e85ed2da7f3e93d7e7f3b9ee482cf3ac8a2
mbucci <mbucci@student.s19.be>
2023-06-12 04:07:34 +0200

    base checks done

--- lib/bonus/ft_atoi_base_bonus.s
+++ lib/bonus/ft_atoi_base_bonus.s
@@ -4,30 +4,85 @@ section .text
 
 ; int ft_atoi_base(const char *str, const char *base);
 _ft_atoi_base:
-	
+	push rbp
+	mov rbp, rsp
 
-.atoi_error:
+	xchg rdi, rsi
+	call _ft_strlen
+	cmp rax, 2
+	jl .base_len_err
+	
+	; allocate 1 ints + 2 char* -> 20 bits
+	sub rsp, 20
+	mov QWORD [rsp], rsi		; s
+	mov QWORD [rsp + 8], rdi	; base
+	mov [rsp + 12], rax			; base_len
+
+	xor rcx, rcx
+	; check base content
+.base_check_loop:
+	cmp BYTE [rel rsp + 8 + rcx], 0
+	je .reset_counter
+	mov dil, BYTE [rel rsp + 8 + rcx]
+	call ft_isspace
+	cmp rax, 0
+	jne .base_invalid
+	cmp BYTE [rel rsp + 8 + rcx], 43	; c == '+'
+	je .base_invalid
+	cmp BYTE [rel rsp + 8 + rcx], 45	; c == '-'
+	je .base_invalid
+	mov sil, BYTE [rel rsp + 8 + rcx]
+	lea rdi, [rel rsp + 8 + rcx + 1]
+	call ft_strchr
+	cmp rax, 0
+	jne .base_invalid
+	inc rcx
+	jmp .base_check_loop
+
+.reset_counter
+	mov rcx, -1
+
+.skip_spaces:
+	inc rcx
+	mov dil, BYTE [rel rsp + rcx]
+	call ft_isspace
+	cmp rax, 0
+	jne .skip_spaces
+
+
+.base_len_err:
 	xor rax, rax
+	pop rbp
 	ret
 
+.base_invalid:
+	xor rax, rax
+	add rsp, 20
+	pop rbp
+	ret
+
+;===================;
 ; int isspace(int c);
+;===================;
 ft_isspace:
-	cmp rdi, 32
+	cmp dil, 32
 	je .isspace_true
-	cmp rdi, 9
+	cmp dil, 9
 	jl .isspace_false
-	cmp rdi, 13
+	cmp dil, 13
 	jg .isspace_false
 
 .isspace_true:
-	mov rax, 1
+	mov eax, 1
 	ret
 
 .isspace_false:
-	xor rax, rax
+	xor eax, eax
 	ret
 
+;======================================;
 ; char *ft_strchr(const char *s, int c);
+;======================================;
 ft_strchr:
 	mov rcx, -1
 
