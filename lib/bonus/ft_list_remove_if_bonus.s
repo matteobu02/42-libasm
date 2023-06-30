c4602cdab91f06cc08e1247d871cb29467eefb2b
Matteo Bucci <mbucci@fu-r3-p8.s19.be>
2023-06-30 18:09:01 +0200

    list_remove_if done but needs testing

--- lib/bonus/ft_list_remove_if_bonus.s
+++ lib/bonus/ft_list_remove_if_bonus.s
@@ -25,28 +25,47 @@ _ft_list_remove_if:
 	mov QWORD [rsp + 16], rcx
 
 .remove_begin:
-	mov r10, QWORD [rsp]		; r10 = *begin_list
-	mov r10, QWORD [r10]
+	mov r10, QWORD [rsp]		; r10 = begin_list
+	mov r10, QWORD [r10]		; r10 = *begin_list
 	cmp r10, 0
 	je .restore_stack
-	mov rdi, QWORD [r10 + 8]	; rdi = r10->data
-	mov rdx, QWORD [rsp + 8]
-	call QWORD [rdx]
-	cmp rax, 0
-	jne .check_done
+	mov rdi, QWORD [r10]		; rdi = r10->data
+	mov rdx, QWORD [rsp + 8]	; rdx = cmp
+	call rdx
+	cmp eax, 0
+	jne .main_remove
 
-	mov r11, QWORD [r10]		; r11 = r10->next
-	mov QWORD [rsp], r11		; *begin_list = (*begin_list)->next
+	mov r11, QWORD [r10 + 8]	; r11 = r10->next
+	lea QWORD [rsp], r11		; *begin_list = r11
 
-	mov rcx, QWORD [rsp + 16]
-	call QWORD [rcx]
+	mov rcx, QWORD [rsp + 16]	; rcx = free_fct
+	call rcx
 	mov rdi, r10
 	call free wrt ..plt
 	jmp .remove_begin
 
+.main_remove:
+	mov r11, QWORD [r10 + 8]	; r11 = r10->next
+	cmp r11, 0
+	je .restore_stack
 
-.check_done:
+	mov rdi, QWORD [r11]		; rdi = r11->data
+	mov rdx, QWORD [rsp + 8]	; rdx = cmp
+	call rdx
+	cmp eax, 0
+	jne .increment_ptr
 	
+	mov rcx, QWORD [r11 + 8]	;
+	mov QWORD [r10 + 8], rcx	; r10->next = r11->next
+
+	mov rcx, QWORD [rsp + 16]	; rcx = free_fct
+	call rcx
+	mov rdi, r11
+	call free wrt ..plt
+
+.increment_ptr:
+	mov r10, QWORD [r10 + 8]
+	jmp .main_remove
 
 .restore_stack:
 	add rsp, 24
