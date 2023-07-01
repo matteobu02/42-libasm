e9d88f899668e8baf5f4992d4cb8246f88cfd1ab
mbucci <mbucci@student.s19.be>
2023-07-01 17:48:24 +0200

    finally finished

--- lib/bonus/ft_list_remove_if_bonus.s
+++ lib/bonus/ft_list_remove_if_bonus.s
@@ -44,12 +44,13 @@ _ft_list_remove_if:
 	cmp eax, 0
 	jne .increment_ptr
 
-	; remove
 	mov rdx, QWORD [r11 + 8]
 	mov QWORD [r10 + 8], rdx	; r10->next = r11->next
+	; remove
 	mov rcx, QWORD [rsp + 16]	; rcx = free_fct
+	push r11
 	call rcx
-	mov rdi, r11
+	pop rdi
 	call free wrt ..plt
 
 .increment_ptr:
@@ -57,22 +58,21 @@ _ft_list_remove_if:
 	jmp .rm_loop
 
 .rm_begin:
-	mov r10, QWORD [rsp]		; r10 = begin_list
-	mov r10, QWORD [r10]		; r10 = *begin_list
-	cmp r10, 0
-	je .restore_stack
+	mov r11, QWORD [rsp]		;
+	mov r10, QWORD [r11]		; r10 = *begin_list
 	mov rdx, QWORD [rsp + 8]	; rdx = cmp
 	mov rdi, QWORD [r10]
 	call rdx
 	cmp eax, 0
 	jne .restore_stack
-	mov rdx, QWORD [rsp + 16]	; rdx = free_fct
-	mov rdi, QWORD [r10]		; rdi = r10->data
-	call rdx
-	mov r11, QWORD [r10 + 8]
-	mov rcx, QWORD [r11]
-	mov QWORD [rsp], rcx
-	mov rdi, r10
+
+	mov rax, QWORD [r10 + 8]	;
+	mov QWORD [r11], rax		; *begin_list = (*begin_list)->next
+
+	mov rcx, QWORD [rsp + 16]	; rdx = free_fct
+	push r10
+	call rcx
+	pop rdi
 	call free wrt ..plt
 
 .restore_stack:
@@ -81,43 +81,3 @@ _ft_list_remove_if:
 
 .endfunc:
 	ret
-
-;.remove_begin:
-;	mov r10, QWORD [rsp]		; r10 = begin_list
-;	mov r10, QWORD [r10]		; r10 = *begin_list
-;	cmp r10, 0
-;	je .restore_stack
-;	mov rdi, QWORD [r10]		; rdi = r10->data
-;	mov rdx, QWORD [rsp + 8]	; rdx = cmp
-;	call rdx
-;	cmp eax, 0
-;	jne .main_remove
-;
-;	mov r11, QWORD [r10 + 8]	; r11 = r10->next
-;	mov rcx, QWORD [r11]
-;	mov QWORD [rsp], rcx		; *begin_list = r11
-;
-;	mov rcx, QWORD [rsp + 16]	; rcx = free_fct
-;	call rcx
-;	mov rdi, r10
-;	call free wrt ..plt
-;	jmp .remove_begin
-;
-;.main_remove:
-;	mov r11, QWORD [r10 + 8]	; r11 = r10->next
-;	cmp r11, 0
-;	je .restore_stack
-;
-;	mov rdi, QWORD [r11]		; rdi = r11->data
-;	mov rdx, QWORD [rsp + 8]	; rdx = cmp
-;	call rdx
-;	cmp eax, 0
-;	jne .increment_ptr
-;	
-;	mov rcx, QWORD [r11 + 8]	;
-;	mov QWORD [r10 + 8], rcx	; r10->next = r11->next
-;
-;	mov rcx, QWORD [rsp + 16]	; rcx = free_fct
-;	call rcx
-;	mov rdi, r11
-;	call free wrt ..plt
