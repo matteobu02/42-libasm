5084801b0939819a8b274a52d1f4ebe102870a01
Matteo Bucci <mbucci@shi-r12-p1.s19.be>
2023-07-01 18:02:06 +0200

    should be working but isnt :(

--- lib/bonus/ft_list_remove_if_bonus.s
+++ lib/bonus/ft_list_remove_if_bonus.s
@@ -12,6 +12,8 @@ section .text
 _ft_list_remove_if:
 	cmp rdi, 0
 	je .endfunc
+	cmp QWORD [rdi], 0
+	je .endfunc
 	cmp rsi, 0
 	je .endfunc
 	cmp rdx, 0
@@ -26,49 +28,52 @@ _ft_list_remove_if:
 	mov QWORD [rsp + 8], rdx
 	mov QWORD [rsp + 16], rcx
 
-.remove_begin:
 	mov r10, QWORD [rsp]		; r10 = begin_list
 	mov r10, QWORD [r10]		; r10 = *begin_list
+
+.rm_loop:
 	cmp r10, 0
-	je .restore_stack
-	mov rdi, QWORD [r10]		; rdi = r10->data
-	mov rdx, QWORD [rsp + 8]	; rdx = cmp
-	call rdx
-	cmp eax, 0
-	jne .main_remove
-
-	mov r11, QWORD [r10 + 8]	; r11 = r10->next
-	mov rcx, QWORD [r11]
-	mov QWORD [rsp], rcx		; *begin_list = r11
-
-	mov rcx, QWORD [rsp + 16]	; rcx = free_fct
-	call rcx
-	mov rdi, r10
-	call _free
-	jmp .remove_begin
-
-.main_remove:
+	je .rm_begin
 	mov r11, QWORD [r10 + 8]	; r11 = r10->next
 	cmp r11, 0
-	je .restore_stack
+	je .rm_begin
 
 	mov rdi, QWORD [r11]		; rdi = r11->data
 	mov rdx, QWORD [rsp + 8]	; rdx = cmp
 	call rdx
 	cmp eax, 0
 	jne .increment_ptr
-	
-	mov rcx, QWORD [r11 + 8]	;
-	mov QWORD [r10 + 8], rcx	; r10->next = r11->next
 
+	mov rdx, QWORD [r11 + 8]
+	mov QWORD [r10 + 8], rdx	; r10->next = r11->next
+	; remove
 	mov rcx, QWORD [rsp + 16]	; rcx = free_fct
+	push r11
 	call rcx
-	mov rdi, r11
+	pop rdi
 	call _free
 
 .increment_ptr:
-	mov r10, QWORD [r10 + 8]
-	jmp .main_remove
+	mov r10, QWORD [r10 + 8]	; r10 = r10->next
+	jmp .rm_loop
+
+.rm_begin:
+	mov r11, QWORD [rsp]		;
+	mov r10, QWORD [r11]		; r10 = *begin_list
+	mov rdx, QWORD [rsp + 8]	; rdx = cmp
+	mov rdi, QWORD [r10]
+	call rdx
+	cmp eax, 0
+	jne .restore_stack
+
+	mov rax, QWORD [r10 + 8]	;
+	mov QWORD [r11], rax		; *begin_list = (*begin_list)->next
+
+	mov rcx, QWORD [rsp + 16]	; rdx = free_fct
+	push r10
+	call rcx
+	pop rdi
+	call _free
 
 .restore_stack:
 	add rsp, 24
@@ -76,3 +81,4 @@ _ft_list_remove_if:
 
 .endfunc:
 	ret
+
